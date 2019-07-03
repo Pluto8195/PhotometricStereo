cd6acd5a61767671f0f6e8a62d01340b4ce3bcbe
Michael Saunders <michael@Michaels-Air.fios-router.home>
2019-07-03 17:59:16 -0400

    cleaned up file structure

--- PSData/Code/NLLSBox/NLLSTest1DCurveTest.m
+++
@@ -1,9 +0,0 @@
-%   Author: Ying Xiong.
-%   Created: Jan 20, 2014.
-
-rng(0);
-
-x = randn();
-CheckGradient(@(a)NLLSTest1DCurve(x, a), 4);
-
-fprintf('Passed.\n');
--- PSData/Code/setPath.m
+++
@@ -1,6 +0,0 @@
-%   Author: Ying Xiong.
-%   Created: Jan 27, 2014.
-
-addpath('DfGBox');
-addpath('NLLSBox');
-addpath('Utils');
